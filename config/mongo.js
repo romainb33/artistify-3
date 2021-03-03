c9fd2768020c24b59c4208b1e0ea2c911ed49236
boubacar Sidibe <bouba.sidibe@gmail.com>
2021-03-03 16:17:03 +0100

    model created

--- config/mongo.js
+++ config/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect("mongodb://localhost/artistify", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
